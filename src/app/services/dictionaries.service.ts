eb159302e296fea29b72389f4da2a450e99f9ea5
Ali Abodaraa <abodaraaali50@gmail.com>
2025-08-10 00:37:50 +0300

    feat(i18n): add Arabic language support for color quiz

--- src/app/services/dictionaries.service.ts
+++ src/app/services/dictionaries.service.ts
@@ -1,42 +1,41 @@
-import { Inject, inject, Injectable } from "@angular/core";
-import { DICTIONARIES_TOKEN } from "../tokens/dictionaries.token";
-import { getDictionary } from "../store/app.helpers";
-import { delay, Observable, of, switchMap, tap, throwError } from "rxjs";
-import { Dictionary } from "../data/dictionaries";
+import { Inject, inject, Injectable } from '@angular/core';
+import { DICTIONARIES_TOKEN } from '../tokens/dictionaries.token';
+import { getDictionary } from '../store/app.helpers';
+import { delay, Observable, of, switchMap, tap, throwError } from 'rxjs';
+import { Dictionary } from '../data/dictionaries';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class DictionariesService {
-    readonly #dictionaries = inject(DICTIONARIES_TOKEN);
-    readonly #delays = [1000, 5000, 'error'];
-    #currentDelayIndex = -1;
+  readonly #dictionaries = inject(DICTIONARIES_TOKEN);
+  readonly #delays = [1000, 3000, 5000, 'error'];
+  #currentDelayIndex = -1;
 
-    readonly languages = Object.keys(this.#dictionaries);
+  readonly languages = Object.keys(this.#dictionaries);
 
-    private dictionaryOf(language: string) {
-        return getDictionary(language, this.#dictionaries);
+  private dictionaryOf(language: string) {
+    return getDictionary(language, this.#dictionaries);
+  }
+
+  private nextDelay() {
+    this.#currentDelayIndex =
+      (this.#currentDelayIndex + 1) % this.#delays.length;
+    return this.#delays[this.#currentDelayIndex];
+  }
+
+  getDictionaryWithDelay(language: string): Observable<Dictionary> {
+    const d = this.nextDelay();
+
+    if (typeof d === 'string') {
+      return of(1).pipe(
+        delay(1000),
+        switchMap((_) => throwError(() => 'Error loading dictionary'))
+      );
+    } else {
+      return of(this.dictionaryOf(language)).pipe(
+        tap((_) => console.log(`Started loading for ${language}`)),
+        delay(d),
+        tap((_) => console.log(`Finished loading for ${language}`))
+      );
     }
-
-    private nextDelay() {
-        this.#currentDelayIndex = (this.#currentDelayIndex + 1) % this.#delays.length;
-        return this.#delays[this.#currentDelayIndex];
-    }
-
-    getDictionaryWithDelay(language: string): Observable<Dictionary> {
-        const d = this.nextDelay();
-
-        if (typeof d === 'string') {
-            return of(1).pipe(
-                delay(1000), 
-                switchMap(_ => throwError(() => 'Error loading dictionary'))
-            )
-        } else {
-            return of(this.dictionaryOf(language)).pipe(
-                tap(_ => console.log(`Started loading for ${language}`)),
-                delay(d),
-                tap(_ => console.log(`Finished loading for ${language}`)),
-            );    
-        }
-        
-
-    }
+  }
 }
